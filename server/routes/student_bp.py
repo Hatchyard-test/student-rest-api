ed3c167aa4b59c98043fbb5a79bc0c1c6120705f
unknown <lsjayawardana95@gmail.com>
2021-06-05 00:06:40 +0530

    Status codes 404 & 500 documented

--- server/routes/student_bp.py
+++ server/routes/student_bp.py
@@ -12,14 +12,34 @@ from controllers.student_controller import (
 
 student = APIRouter() 
 
+responses = {
+        404: {
+            "description": "Error: Not Found",
+            "content": {
+                "application/json": {
+                    "example": {"details": "Item not found"}
+                }
+            }
+        },
+        500: {
+            "description": "Error: Internal Server Error",
+            "content": {
+                "application/json": {
+                    "example": "Internal Server Error"
+                }
+            },
+        },
+}
+
+
 # Endpoints routes for api
-student.get('/', response_description="Students retrieved")(retrieve_students_data)
+student.get('/',responses=responses, response_description="Students retrieved")(retrieve_students_data)
 
-student.get('/{id}',response_model = StudentResponseModel, response_description="Student data retrieved")(retrieve_student_data)
+student.get('/{id}',responses=responses,response_model = StudentResponseModel, response_description="Student data retrieved")(retrieve_student_data)
 
-student.post('/',response_model = StudentResponseModel, status_code=201, response_description="Student data added into database")(add_student_data)
+student.post('/', status_code=201,responses=responses,response_model = StudentResponseModel, response_description="Student data added into database")(add_student_data)
 
-student.put('/{id}',response_model = StudentResponseModel, response_description="Student data update sucessfully")(update_student_data)
+student.put('/{id}',responses=responses,response_model = StudentResponseModel, response_description="Student data update sucessfully")(update_student_data)
 
-student.delete('/{id}', status_code=204, response_description="Student data sucessfully deleted from the database")(delete_student_data)
+student.delete('/{id}', status_code=204,responses=responses, response_description="Student data sucessfully deleted from the database")(delete_student_data)
 
